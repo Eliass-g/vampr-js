3b5392fe52ecd93fb621cc82ccc73981850dec3b
Eliass Ghauss <eliass.ghauss@gmail.com>
2023-10-07 20:43:52 -0400

    vamp p1

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let vampire = this;
+    let result = 0;
+    while (vampire.creator) {
+      vampire = vampire.creator;
+      result++;
+    }
+    return result;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const val1 = this.numberOfVampiresFromOriginal;
+    const val2 = vampire.numberOfVampiresFromOriginal;
+    if (val1 < val2) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,8 +48,20 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
-  }
+    let val1 = this;
+    let val2 = vampire;
+    while (val1.isMoreSeniorThan(val2)) {
+      val2 = val2.creator;
+    }
+    while (val2.isMoreSeniorThan(val1)) {
+      val1 = val1.creator;
+    }
+    while (val1 !== val2) {
+      val1 = val1.creator;
+      val2 = val2.creator;
+    }
+    return val1;
+  } 
 }
 
 module.exports = Vampire;
